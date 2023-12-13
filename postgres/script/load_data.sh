9d7aa419c9b6c1418cdcb7cfb7c938999c94dd66
Daniel Alvarez <dalvarez@ilab.net>
2023-12-13 02:11:52 -0600

    feat: first commit, functional version

--- postgres/script/load_data.sh
+++ postgres/script/load_data.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Configura tus variables
+
 SCHEMA='test'
 TABLE='users'
 CSV_PATH='/users_data.csv'
 
-# Define las columnas del CSV que se mapearán a la tabla (todas excepto 'id')
+
 COLUMNS='first_name, last_name, company_name, address, city, state, zip, phone1, phone2, email, department'
 
-# Comando SQL para COPY
+
 SQL="\copy ${SCHEMA}.${TABLE}(${COLUMNS}) FROM '${CSV_PATH}' DELIMITER ',' CSV HEADER;"
 
-# Ejecuta el comando
+
 PGPASSWORD="$POSTGRES_PASSWORD" psql -h "${POSTGRES_HOST}" -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" -c "$SQL"
